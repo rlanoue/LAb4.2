43381a503745066388845eab30c90726889b416c
Jordan Lanoue <rlanoue@ou.edu>
2019-02-08 22:54:48 -0600

    Added ZooTest.java

--- ZooTest.java
+++ ZooTest.java
@@ -3,16 +3,24 @@ public class ZooTest {
 	    /**
 	     * Test the empty Animal constructor and the getters
 	     */
+	
+ 	Zoo zooCapactity = new Zoo(81);
+ 	Animal animal1 = new Animal("grey", "Ryan-Goosling", 365.0, 84); 
+ 		public void capacityTest() throws AssertException
+ 		{
+ 			 Assert.assertEquals(zooCapactity.getCapacity(), 81);
+ 		}
+ 	
+ 		public void getTotalHeight() throws AssertException
+ 		{
+ 			zooCapactity.addAnimal(animal1); 
+ 			Assert.assertEquals(zooCapactity.getTotalHeight(), 84, 0.01);
+ 		}
+ 	
 	   public void toStringTest() throws AssertException
-	     {
-	    	Animal testAnimal2 = new Animal();
+	     {  
+	    	String expectedFormat = "Ryan-Goosling, a grey-colored animal. 365.0 pounds, 84.0 inches\n"; 
 	    	
-	    	String expectedFormat =  "Umbrella, 314, 469 lbs, Price: $92.67 - %s",
-	                name, count, totalWeight, totalPrice, description);
-	    	//"%s, a %s-colored animal. %.1f pounds, %.1f inches\n
-	    	
-	    	Assert.assertEquals(expectedFormat, testAnimal2.toString()); 
-	     
-	     getTotalHeight; 
-	     get Capactity Test; 
+	    	Assert.assertEquals(expectedFormat, animal1.toString());
 	     }
+}
